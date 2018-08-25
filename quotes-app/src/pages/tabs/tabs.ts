b57a04be57bb751351bc6e195e7bdf3f6de3285e
Kusal Kankanamge <kusald.14@cse.mrt.ac.lk>
2018-08-25 18:31:12 +0530

    Favourites and Library tabs added

--- quotes-app/src/pages/tabs/tabs.ts
+++ quotes-app/src/pages/tabs/tabs.ts
@@ -1,14 +1,17 @@
 import {Component} from "@angular/core";
 import {FavouritesPage} from "../favourites/favourites";
+import {LibraryPage} from "../library/library";
 
 @Component({
     selector: 'pages-tab',
     template: `
         <ion-tabs>
-            <ion-tab [root]="favouritesPage"></ion-tab>
+            <ion-tab [root]="favouritesPage" tabTitle="Favourites" tabIcon="star"></ion-tab>
+            <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
         </ion-tabs>
     `
 })
 export class TabsPage {
     favouritesPage = FavouritesPage;
+    libraryPage = LibraryPage;
 }
