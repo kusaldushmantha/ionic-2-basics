b15c95159f32d27c53a10ce4c4ae1175bf1664bb
Kusal Kankanamge <kusald.14@cse.mrt.ac.lk>
2018-08-26 11:14:20 +0530

    Modal overlay to display individual quotes in favourites added

--- quotes-app/src/pages/favourites/favourites.ts
+++ quotes-app/src/pages/favourites/favourites.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Quote} from "../../data/quote.interface";
 import {QuotesService} from "../../services/quotes";
+import {ModalController} from "ionic-angular";
+import {QuotePage} from "../quote/quote";
 
 @Component({
   selector: 'page-favourites',
@@ -8,12 +10,18 @@ import {QuotesService} from "../../services/quotes";
 })
 export class FavouritesPage {
 
-  constructor(private quoteService: QuotesService) {}
+  constructor(private quoteService: QuotesService,
+               private modalCtrl: ModalController) {}
 
   quotes: Quote[];
 
   ionViewWillEnter(){
     this.quotes = this.quoteService.getFavouriteQuote();
+  }
+
+  onViewQuote(quote: Quote){
+     const modal = this.modalCtrl.create(QuotePage, quote);
+     modal.present();
   }
 
 }
--- quotes-app/src/pages/quote/quote.ts
+++ quotes-app/src/pages/quote/quote.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NavParams, ViewController} from "ionic-angular";
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private viewCtrl: ViewController,
+               private navParams: NavParams){}
+
+  person: string;
+  text: string;
+
+  ionViewDidLoad(){
+    this.person = this.navParams.get('person');
+    this.text = this.navParams.get('text');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose(){
+    this.viewCtrl.dismiss();
   }
 
 }
