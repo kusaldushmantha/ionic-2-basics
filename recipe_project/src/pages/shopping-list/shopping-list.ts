063a75ea7abfbe80251ed157e29425c5051f016f
Kusal Kankanamge <kusald.14@cse.mrt.ac.lk>
2018-09-08 17:07:15 +0530

    Displaying shopping list added

--- recipe_project/src/pages/shopping-list/shopping-list.ts
+++ recipe_project/src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {ShoppingListService} from "../../services/shopping-list";
+import {Ingredient} from "../../models/ingredient";
 
 @IonicPage()
 @Component({
@@ -15,9 +16,20 @@ export class ShoppingListPage {
                public shoppingListService: ShoppingListService) {
   }
 
+  itemList: Ingredient[];
+
+  ionViewWillEnter() {
+    this.itemList = this.shoppingListService.getItems();
+  }
+
   onAddItem(form: NgForm) {
     this.shoppingListService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.itemList = this.shoppingListService.getItems();
   }
 
 }
