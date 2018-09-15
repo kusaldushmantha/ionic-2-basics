d8ad45d6eb4209a49b4b8956158aa0a311ccb239
Kusal Kankanamge <kusald.14@cse.mrt.ac.lk>
2018-09-15 19:50:32 +0530

    ActionSheet Tutorial Added

--- recipe_project/src/pages/edit/edit.ts
+++ recipe_project/src/pages/edit/edit.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActionSheetController, AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -14,7 +14,9 @@ export class EditPage {
   recipeForm: FormGroup;
 
   constructor(public navCtrl: NavController,
-               public navParams: NavParams) {
+               public navParams: NavParams,
+               public actionSheetCtrl: ActionSheetController,
+               public alertCtrl: AlertController) {
 
     this.mode = this.navParams.get('mode');
     this.initializeForm();
@@ -24,12 +26,73 @@ export class EditPage {
     this.recipeForm = new FormGroup({
         'title': new FormControl(null, Validators.required),
         'description': new FormControl(null, Validators.required),
-        'difficulty': new FormControl(this.selectOptions[1], Validators.required)
+        'difficulty': new FormControl(this.selectOptions[1], Validators.required),
+        'ingredients': new FormArray([])
     });
   }
 
   onSubmit() {
       console.log(this.recipeForm);
+  }
+
+  private createNewIngredientAlert() {
+      return this.alertCtrl.create({
+        title: 'Add Ingredient',
+        inputs: [
+            {
+                name: 'name',
+                placeholder: 'Name'
+            }
+        ],
+        buttons: [
+            {
+                text: 'Cancel',
+                role: 'cancel'
+            },
+            {
+                text: 'Add',
+                handler: data => {
+                    if(data.name.trim() == '' || data.name == null){
+                        return;
+                    }
+                    (<FormArray>this.recipeForm.get('ingredients'))
+                        .push(new FormControl(data.name, Validators.required))
+                }
+            }
+        ]
+      });
+  }
+
+  onManageIngredients() {
+      const actionSheet = this.actionSheetCtrl.create({
+         title: 'What do you want to do?',
+         buttons: [
+             {
+                 text: 'Add Ingredients',
+                 handler: () => {
+                    this.createNewIngredientAlert().present();
+                 }
+             },
+             {
+                 text: 'Remove all Ingredients',
+                 role: 'destructive',
+                 handler: () => {
+                     const formArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+                     const len = formArray.length;
+                     if(len > 0){
+                         for(let i = len - 1; i >= 0; i--) {
+                             formArray.removeAt(i);
+                         }
+                     }
+                 }
+             },
+             {
+                 text: 'Cancel',
+                 role: 'cancel'
+             }
+         ]
+      });
+      actionSheet.present();
   }
 
 }
